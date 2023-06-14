1545e0d5393abd6d0019c865118d63f2b31087ae
karoliskersys <kersys.karolis@inbox.lt>
2023-06-14 18:29:05 +0300

    REST-API Account entity class created

--- src/main/java/lt/socialmedia/socialmedia/entity/Account.java
+++ src/main/java/lt/socialmedia/socialmedia/entity/Account.java
@@ -1,15 +1,31 @@
 package lt.socialmedia.socialmedia.entity;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Getter;
 import lombok.Setter;
 
-@AllArgsConstructor
+import java.time.LocalDateTime;
+
+
+
 @Getter
 @Setter
 public class Account {
+    private static Long index = 0L;
     private Long id;
     private String name;
     private String lastname;
     private Integer age;
+     private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Account (String name, String lastname, Integer age){
+        this.id = ++index;
+        this.name = name;
+        this.lastname = lastname;
+        this.age = age;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
 }
